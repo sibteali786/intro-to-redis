de0f1b1f12db90f7ec2537c88952b812e94ff2d8
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-10-03 15:18:18 +0500

    simulate postgres cache query

--- server.js
+++ server.js
@@ -11,14 +11,46 @@ client.on("error", (err) => console.error("Redis Client Error", err));
   await client.connect(); // Connect to Redis
   console.log("Connected to Redis");
 })();
-
+function cache(key, ttl, slowFn) {
+  return async function cachedFn(...props) {
+    const cachedResponse = await client.get(key);
+    if (cachedResponse) {
+      return cachedResponse;
+    }
+    const result = await slowFn(...props);
+    await client.setEx(key, ttl, result);
+    return result;
+  };
+}
+async function verySlowAndExpensivePostgresSQLQuery() {
+  console.log("an expensive query");
+  const promise = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(new Date().toUTCString());
+    }, 5000);
+  });
+  return promise;
+}
+const cachedFn = cache(
+  "expensive_call",
+  10,
+  verySlowAndExpensivePostgresSQLQuery,
+);
 async function init() {
   const app = express();
   const PORT = 3000;
 
   // Serve static files from the "static" directory
   app.use(express.static("./static"));
-
+  app.get("/get", async (req, res) => {
+    const data = await cachedFn();
+    res
+      .json({
+        status: "ok",
+        data,
+      })
+      .end();
+  });
   // Define the /pageview route
   app.get("/pageview", async (req, res) => {
     try {
